b46ba6f4c88394ba8fb0d4e9a75f0663514a7d2f
AencioVincius2 <aenciosilva@gmail.com>
2025-06-18 20:27:36 -0300

    add: tupla

---
+++ py_aulas/tupla.py
@@ -0,0 +1,13 @@
+filmsTupla = ("Inception","The Shawshank Redemption","The Dark Origin")
+
+print(type(filmsTupla))
+
+print(filmsTupla[:2])
+
+print(filmsTupla[:-1])
+
+print(filmsTupla[:3])
+
+print(filmsTupla[1:])
+
+print(filmsTupla.index("Inception"))
