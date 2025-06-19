a417c8a3ac218861b364fc8d768d5e4e527ae9c4
Aencio <aenpvp9@gmail.com>
2025-06-19 19:14:44 -0300

    add: listas

---
+++ py_aulas/listas.py
@@ -0,0 +1,27 @@
+film_matrix = ["Matrix",1999,8.7,True]
+
+print(film_matrix)
+
+filmes_list = ["Inception","The Shawshank Redemption","The Dark Origin"]
+
+print(len(filmes_list))
+
+print(filmes_list.index("Inception"))
+
+print(filmes_list[:2])
+
+filmes_list.append("The lord of the rings")
+
+print(filmes_list)
+
+filmes.sort()
+
+print(filmes_list)
+
+filmes_copy = filmes_list.copy()
+
+print(filmes_copy)
+
+filmes_copy.clear()
+
+print(filmes_copy)
---
+++ py_aulas/operador_ternario.py
@@ -0,0 +1,11 @@
+"""
+Operador ternário em Python
+"""
+logged_user = False
+
+if logged_user:
+    msg = 'Usuário logado'
+else:
+    msg = 'Usuário precisa logar.'
+
+print(msg)
