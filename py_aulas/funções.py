000374a55a19695f4966236a80bbca1005c43e21
Aencio <aenpvp9@gmail.com>
2025-06-20 14:14:29 -0300

    add: funções

---
+++ py_aulas/funções.py
@@ -0,0 +1,19 @@
+def welcome():
+    print("bienvenido")
+
+#welcome()
+
+def calculate_average()
+    num_ratings = int(input("Digite quantas avaliações deseja fazer pro filme:\n"))
+    total = 0
+    for i in range(num_ratings) = float(input("Digite a nota para o filme:\n"))
+    total +=
+    
+    if num_ratings > 0:
+        average = total / num_ratings
+    else:
+        average = 0
+        
+    return average
+
+print(f"A média de avaliações é: {calculate_average():.2f}")
