0e59c553b857dac1a811f60be028d6b4d3aed795
Aencio <aenpvp9@gmail.com>
2025-06-22 19:34:42 -0300

    add: objetos

---
+++ py_aulas/objsts/objetos.py
@@ -0,0 +1,17 @@
+class Game:
+    name = ""
+    yearLaunch = ""
+    multiplayer = ""
+    note = 0
+
+#Primeiro jogo
+
+game1 = Game()
+
+game1.name = "Gta VI"
+game1.yearLaunch = "2026"
+game1.multiplayer = "Yes"
+game1.note = 8.5
+
+print("###Dados do jogo###")
+print(f"Nome do jogo: {game1.name}\nAno de lançamento: {game1.yearLaunch}")
