9ac8baf7393ba2e506e2abecdf74c73f434b88a4
AencioVincius2 <aenciosilva@gmail.com>
2025-06-23 17:35:06 -0300

    feat:path parameter

--- aulasFastApi/main.py
+++ aulasFastApi/main.py
@@ -2,7 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+jogadores = {
+    1: {
+        "nome":"Rony",
+        "idade":26,
+        "Time":"Palmeiras"
+    },
+    2: {
+        "nome":"Gustavo",
+        "idade":29,
+        "Time":"Palmeiras"
+    }
+}
+##uvicorn main:app --reload
 @app.get("/")
 def inicio():
-    return {"Hello": "World"}
+    return jogadores
+
+#get-jogadores/1 - Path Parameter
+@app.get("/get-jogador/{id_jogador}")
+def get_jogador(id_jogador:int):
+    return jogadores[id_jogador]
+
+#get-jogadores/?id=1 - Query Parameter
