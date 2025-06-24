f44b93f4d1a0a06193a1a2a386cd545554305335
Aencio <aenpvp9@gmail.com>
2025-06-24 11:40:50 -0300

    add: property

--- py_aulas/objsts/praticando/biblioteca.py
+++ py_aulas/objsts/praticando/biblioteca.py
@@ -1,15 +1,27 @@
 class Biblioteca(object):
+    bibliotecas = []
     def __init__ (self, nome, ativo):
         self.nome = nome
         self.ativo = ativo
+        Biblioteca.bibliotecas.append(self)
+    def __str__(self):
+        return self.nome
     
+    def listar_blibliotecas():
+        for i in Biblioteca.bibliotecas:
+            print(bibliotecas.nome)
+    def alterna_estado(self):
+        self.ativo = not self.ativo
+    
+    @property
+    def ativo(self):
+        return "ativada" if self.ativo else "desativada"
+                    
+        
+biblioteca_cidade = Biblioteca("Biblioteca da cidade", True)
+biblioteca_shopping = Biblioteca("Biblioteca do shopping", True)
+print(biblioteca_cidade)
 
-biblioteca_cidade = Biblioteca('Biblioteca da cidade', True)
-biblioteca_shopping = Biblioteca('Biblioteca do shopping', True)
-bibliotecas = [biblioteca_cidade, biblioteca_shopping]
+Biblioteca.listar_blibliotecas()
 
-print("----------------------------------------------------------------------------------------------------------------------------------")
-for tecas in bibliotecas:
-    print(vars(tecas))
-print("----------------------------------------------------------------------------------------------------------------------------------")    
-print(vars(biblioteca_cidade))
+    
