d29e1a61bb9390fe6b1a99fd1e40c96d3ea500a4
AencioVincius2 <aenciosilva@gmail.com>
2025-06-23 23:43:50 -0300

    add: projetofastapi01

--- aulasFastApi/main.py
+++ aulasFastApi/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+#usado para manipular e validar dados simples @see https://docs.pydantic.dev/latest/#pydantic-examples
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -14,6 +17,17 @@ jogadores = {
         "time":"Palmeiras"
     }
 }
+
+class Jogador(BaseModel):
+    nome: str
+    idade: int
+    time: str
+
+class Atualiza_Jogador(BaseModel):
+    nome: Optional[str] = None
+    idade: Optional[int] = None
+    time: Optional[str] = None
+
 ##uvicorn main:app --reload
 @app.get("/")
 def inicio():
@@ -24,13 +38,37 @@ def inicio():
 def get_jogador(id_jogador:int):
     return jogadores[id_jogador]
 
+#get-jogadores/?id=1 - Query Paramete
+#get-jogador-time?time="nometime"
 @app.get("/get-jogador-time")
 def get_jogador_time(time:str):
     for jogador_id in jogadores:
         if jogadores[jogador_id]["time"] == time:
             return jogadores[jogador_id]
     return {"Dados": "Não foi encontrado"}
-    
 
-#get-jogadores/?id=1 - Query Parameter
-#get-jogador-time?time="nometime"
+@app.post("/cadastra-jogador/{jogador_id}")
+def cadastra_jogador(jogador_id: int, jogador:Jogador):
+    if jogador_id in jogadores:
+        return {"Erro":"Jogador já existe"}
+    jogadores[jogador_id] = jogador
+    return jogadores[jogador_id]
+
+@app.put("/atualiza-jogador{jogador_id}")
+def atualiza_jogadores(jogador_id: int, jogador: Atualiza_Jogador):
+    if not jogador_id in jogadores:
+        return {"Erro":"Jogador não existe"}
+    if jogador.nome != None:
+        jogadores[jogador_id]["nome"] = jogador.nome
+    if jogador.idade != None:
+        jogadores[jogador_id]["idade"] = jogador.idade
+    if jogador.time != None:
+        jogadores[jogador_id]["time"] = jogador.time
+    return jogadores[jogador_id]
+
+@app.delete("/deleta-jogador{jogador_id}")
+def deleta_jogadores(jogador_id: int):
+    if not jogador_id in jogadores:
+        return {"Erro":"Jogador não existe"}
+    jogadores.pop(jogador_id)
+    return {"Mensagem:Jogador deletado!"}
