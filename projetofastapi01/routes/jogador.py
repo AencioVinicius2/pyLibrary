c9bce9ce1dc8654be833942d14f5f90913657472
Aencio <aenpvp9@gmail.com>
2025-06-28 21:14:46 -0300

    add: detalhes jogador

--- projetofastapi01/routes/jogador.py
+++ projetofastapi01/routes/jogador.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from config.database import conexao
 from models.jogador import Jogador
 from schemas.jogador import jogador_entidade, lista_jogadores_entidade
+from bson import ObjectId
 
 jogador_router = APIRouter()
 
@@ -9,11 +10,19 @@ jogador_router = APIRouter()
 async def inicio():
     return {"Mensagem":"Teste"}
 
+# Listar jogadores
 @jogador_router.get('/jogadores')
 async def lista_jogadores():
     return lista_jogadores_entidade(conexao.local.jogador.find())
 
-#insere novos jogadores
+# Detalhe de um jogador
+@jogador_router.get('/jogador/{jogador_id}')
+def buscar_jogador_id(jogador_id):
+    return jogador_entidade(conexao.local.jogador.find_one(
+        {"_id": ObjectId(jogador_id)}
+    ))
+
+# insere novos jogadores
 @jogador_router.post('/jogadores')
 async def cadastra_jogadores(jogador: Jogador):
     conexao.local.jogador.insert_one(dict(jogador))
