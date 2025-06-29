70ffa5cc229e82c60c5566d010542bc812b2bcbd
Aencio <aenpvp9@gmail.com>
2025-06-28 21:55:16 -0300

    add: put e delete

--- projetofastapi01/routes/jogador.py
+++ projetofastapi01/routes/jogador.py
@@ -4,6 +4,8 @@ from models.jogador import Jogador
 from schemas.jogador import jogador_entidade, lista_jogadores_entidade
 from bson import ObjectId
 
+#@see https://www.mongodb.com/docs/languages/python/pymongo-driver/current/crud/
+
 jogador_router = APIRouter()
 
 @jogador_router.get("/")
@@ -17,7 +19,7 @@ async def lista_jogadores():
 
 # Detalhe de um jogador
 @jogador_router.get('/jogador/{jogador_id}')
-def buscar_jogador_id(jogador_id):
+async def buscar_jogador_id(jogador_id):
     return jogador_entidade(conexao.local.jogador.find_one(
         {"_id": ObjectId(jogador_id)}
     ))
@@ -27,3 +29,32 @@ def buscar_jogador_id(jogador_id):
 async def cadastra_jogadores(jogador: Jogador):
     conexao.local.jogador.insert_one(dict(jogador))
     return lista_jogadores_entidade(conexao.local.jogador.find())
+
+#atualiza jogador
+@jogador_router.put('/jogadores/{jogador_id}')
+async def atualiza_jogador(jogador_id, jogador: Jogador):
+    conexao.local.jogador.find_one_and_update(
+        {
+            "_id": ObjectId(jogador_id)
+        },
+        {
+            "$set": dict(jogador)
+        }
+    )
+    return jogador_entidade(
+        conexao.local.jogador.find_one(
+            {
+                "_id": ObjectId(jogador_id)
+            }
+        )
+    )
+
+#deleta jogador
+@jogador_router.delete('/jogador/{jogador_id}')
+async def deleta_jogador(jogador_id):
+    conexao.local.jogador.delete_one(
+        {
+            "_id": ObjectId(jogador_id)
+        }
+    )
+    return {"Mensagem":"Jogador Deletado!"}
